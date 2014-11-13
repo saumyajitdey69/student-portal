5ca755860da0eaec138eaf3fea0d76d23a571185
Vaibhav Awachat <awachat11vaibhav@gmail.com>
2014-11-14 03:36:32 +0530

    readme updated, new schema updated

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-academic_audit
-==============
+Student Portal
+
+How to setup
+1. Upload assets/sql/wsdc_student into mysql database
+2. copy application/config/database_example.php 
+3. rename copy of application/config/database_example.php to application/config/database.php
+4. enter appropriate mysql username and database name
+
+
+How to login to portal
+NOTE: A default user is already created
+
+1. username: ABHISHEK
+2. password: password
--- assets/sql/wsdc_student.sql
+++ assets/sql/wsdc_student.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.11
+-- version 3.5.2.2
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Nov 05, 2014 at 03:54 PM
--- Server version: 5.5.16
--- PHP Version: 5.3.8
+-- Host: 127.0.0.1
+-- Generation Time: Nov 13, 2014 at 11:01 PM
+-- Server version: 5.5.27
+-- PHP Version: 5.4.7
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `wsdc_student`
+-- Database: `wsdc_test_db`
 --
 
 -- --------------------------------------------------------
@@ -26,6 +26,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `groups`
 --
 
+DROP TABLE IF EXISTS `groups`;
 CREATE TABLE IF NOT EXISTS `groups` (
   `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL,
@@ -38,8 +39,8 @@ CREATE TABLE IF NOT EXISTS `groups` (
 --
 
 INSERT INTO `groups` (`id`, `name`, `description`) VALUES
-(1, 'admin', 'Administrator'),
-(2, 'members', 'General User');
+(1, 'admin', 'WSDC  Administrator'),
+(2, 'member', 'Normal Sutdent');
 
 -- --------------------------------------------------------
 
@@ -47,6 +48,7 @@ INSERT INTO `groups` (`id`, `name`, `description`) VALUES
 -- Table structure for table `login_attempts`
 --
 
+DROP TABLE IF EXISTS `login_attempts`;
 CREATE TABLE IF NOT EXISTS `login_attempts` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `ip_address` varchar(15) NOT NULL,
@@ -61,43 +63,32 @@ CREATE TABLE IF NOT EXISTS `login_attempts` (
 -- Table structure for table `student_data`
 --
 
+DROP TABLE IF EXISTS `student_data`;
 CREATE TABLE IF NOT EXISTS `student_data` (
   `userid` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
-  `name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
   `roll_number` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
   `registration_number` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `current_section` varchar(4) COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
-  `current_year` varchar(4) COLLATE utf8_unicode_ci NOT NULL,
   `joining_year` varchar(4) COLLATE utf8_unicode_ci NOT NULL,
   `course` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
   `branch` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
   `gender` char(1) COLLATE utf8_unicode_ci NOT NULL,
-  `email` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
   `birthday` date NOT NULL,
   `country` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
-  `mobile` varchar(16) COLLATE utf8_unicode_ci NOT NULL,
   `emergency_contact` varchar(16) COLLATE utf8_unicode_ci NOT NULL,
   `sbh_account` varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL,
   `passport` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `hostel_room` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
-  `hostel` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
-  `mess` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
-  `created_location` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
-  `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`userid`),
-  UNIQUE KEY `username` (`username`),
-  UNIQUE KEY `email` (`email`),
   UNIQUE KEY `roll_number` (`roll_number`),
   UNIQUE KEY `registration_number` (`registration_number`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=24 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1587 ;
 
 --
 -- Dumping data for table `student_data`
 --
 
-INSERT INTO `student_data` (`userid`, `username`, `name`, `roll_number`, `registration_number`, `current_section`, `current_year`, `joining_year`, `course`, `branch`, `gender`, `email`, `birthday`, `country`, `mobile`, `emergency_contact`, `sbh_account`, `passport`, `hostel_room`, `hostel`, `mess`, `created_location`, `created_time`) VALUES
-(1, 'admin', 'ABHISHEK SINGH', '117203', 'A11019', 'B', '', '2011', 'btech', 'cse', 'M', 'abhi15081993@gmail.com', '1993-08-15', 'INDIAN', '8121210825', '9258781754', '62194941921', '', '0', '0', '0', '', '2014-11-04 14:51:37');
+INSERT INTO `student_data` (`userid`, `roll_number`, `registration_number`, `joining_year`, `course`, `branch`, `gender`, `birthday`, `country`, `emergency_contact`, `sbh_account`, `passport`, `timestamp`) VALUES
+(1, '117203', 'A11019', '2011', 'btech', 'cse', 'M', '1993-08-15', 'INDIAN', '9258781754', '62194941921', '', '2014-11-13 21:59:27');
 
 -- --------------------------------------------------------
 
@@ -105,6 +96,7 @@ INSERT INTO `student_data` (`userid`, `username`, `name`, `roll_number`, `regist
 -- Table structure for table `users`
 --
 
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `ip_address` varchar(15) NOT NULL,
@@ -125,7 +117,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `company` varchar(100) DEFAULT NULL,
   `phone` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=24 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1587 ;
 
 --
 -- Dumping data for table `users`
@@ -140,6 +132,7 @@ INSERT INTO `users` (`id`, `ip_address`, `username`, `password`, `profile_edited
 -- Table structure for table `users_groups`
 --
 
+DROP TABLE IF EXISTS `users_groups`;
 CREATE TABLE IF NOT EXISTS `users_groups` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(11) unsigned NOT NULL,
@@ -148,7 +141,7 @@ CREATE TABLE IF NOT EXISTS `users_groups` (
   UNIQUE KEY `uc_users_groups` (`user_id`,`group_id`),
   KEY `fk_users_groups_users1_idx` (`user_id`),
   KEY `fk_users_groups_groups1_idx` (`group_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=57 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1631 ;
 
 --
 -- Dumping data for table `users_groups`
@@ -157,17 +150,6 @@ CREATE TABLE IF NOT EXISTS `users_groups` (
 INSERT INTO `users_groups` (`id`, `user_id`, `group_id`) VALUES
 (8, 1, 1),
 (9, 1, 2);
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `users_groups`
---
-ALTER TABLE `users_groups`
-  ADD CONSTRAINT `fk_users_groups_groups1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_users_groups_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
