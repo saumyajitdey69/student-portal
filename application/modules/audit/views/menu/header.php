e4696ce4b060a4862c30d8522911305fd4d374a1
Vaibhav Awachat <awachat11vaibhav@gmail.com>
2015-01-01 01:38:18 +0530

    side menu of audit updated

--- application/modules/audit/views/menu/header.php
+++ application/modules/audit/views/menu/header.php
@@ -8,11 +8,26 @@
       <li class="list-group-item <?php echo $current_page === "profile" ? "active" : ""?>">
         <a href="<?php echo base_url('audit/profile'); ?>">Profile</a>
       </li>
-      <li class="list-group-item <?php echo $current_page === "attendance" ? "active" : ""?>"> 
-        <a href="<?php echo base_url('attendance'); ?>">Attendance(2014-15 Odd sem)</a>
-      </li>
-      <li class="list-group-item <?php echo $current_page === "attendance_even" ? "active" : ""?>"> 
-        <a href="<?php echo base_url('attendance/even_sem14_15'); ?>">Attendance(2014-15 Even sem)<span class="label label-danger"> New</span></a>
+      <li class="list-group-item">
+        <span class="collapse-caret collapsed dropdown-toggle" data-toggle="collapse" data-target="#attendance"></span>
+        <div class="list-group-item-wrapper">
+          <a href="#">Attendance</a>
+        </div>
+
+        <div id="attendance" class="collapse">
+          <ul role="menu" class="list-group-item-menu">
+            <li class="list-group-item">
+              <div class="list-group-item-wrapper">
+                <a href="<?php echo base_url('attendance/even_sem14_15'); ?>">Even Semester</a>
+              </div>
+            </li>
+            <li class="list-group-item">
+              <div class="list-group-item-wrapper">
+                <a href="<?php echo base_url('attendance'); ?>">Odd Semester</a>
+              </div>
+            </li>
+          </ul>
+        </div>
       </li>
       <li class="list-group-item <?php echo $current_page === "feedback" ? "active" : ""?>">
        <a href="<?php echo base_url('audit/feedback'); ?>">Feedback</a>
@@ -27,15 +42,26 @@
       Results
     </a>
   </li>
-  <li  class="list-group-item <?php echo $current_page === "slip" ? "active" : ""?>" >
-    <a href="<?php echo base_url('audit/slip'); ?>">
-      Registration Slip(2014-15 Odd sem)
-    </a>
-  </li>
-  <li  class="list-group-item <?php echo $current_page === "slip_even" ? "active" : ""?>" >
-    <a href="<?php echo base_url('audit/slip/even_sem14_15'); ?>">
-      Registration Slip(2014-15 Even sem)<span class="label label-danger"> New</span>
-    </a>
+  <li class="list-group-item">
+    <span class="collapse-caret collapsed dropdown-toggle" data-toggle="collapse" data-target="#registration-slip"></span>
+    <div class="list-group-item-wrapper">
+      <a href="#">Registration Slip</a>
+    </div>
+
+    <div id="registration-slip" class="collapse">
+      <ul role="menu" class="list-group-item-menu">
+        <li class="list-group-item">
+          <div class="list-group-item-wrapper">
+            <a href="<?php echo base_url('audit/slip/even_sem14_15'); ?>">Even Semester</a>
+          </div>
+        </li>
+        <li class="list-group-item">
+          <div class="list-group-item-wrapper">
+            <a href="<?php echo base_url('audit/slip'); ?>">Odd Semester</a>
+          </div>
+        </li>
+      </ul>
+    </div>
   </li>
   <li  class="list-group-item <?php echo $current_page === "calendar" ? "active" : ""?>">
     <a href="<?php echo base_url('audit/calendar'); ?>">
