225c3c9e0756484f57bf1d9aeba37e6e83e2911d
Vaibhav Awachat <awachat11vaibhav@gmail.com>
2015-01-03 05:38:05 +0530

    radio content updated

--- assets/js/jplayer/config.js
+++ assets/js/jplayer/config.js
@@ -1,20 +1,57 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-	$("#jquery_jplayer_1").jPlayer({
-		ready: function (event) {
-			$(this).jPlayer("setMedia", {
-				title: "NITW LAN Radio - English ",
-				mp3:"http://localhost/gitlab/student-portal/assets/js/jplayer/lanradio.wvx"
-			});
-		},
-		swfPath: "",
-		supplied: "mp3",
-		wmode: "window",
-		useStateClassSkin: true,
-		autoBlur: false,
-		smoothPlayBar: true,
-		keyEnabled: true,
-		remainingDuration: true,
-		toggleDuration: true
-	});
+    $("#jquery_jplayer_1").jPlayer({
+        ready: function(event) {
+            $(this).jPlayer("setMedia", {
+                title: "NITW LAN Radio - English ",
+                mp3: "http://172.30.152.34:8000/stream/1/"
+            });
+        },
+        swfPath: "",
+        supplied: "mp3",
+        wmode: "window",
+        useStateClassSkin: true,
+        autoBlur: false,
+        smoothPlayBar: true,
+        keyEnabled: true,
+        remainingDuration: true,
+        toggleDuration: true
+    });
 });
+
+var uiBlocked = false;
+
+window.onload = checkServer();
+
+function checkServer() {
+    $.ajax({
+        cache: false,
+        type: 'GET',
+        url: url,
+        timeout: 1000,
+        success: function(data, textStatus, XMLHttpRequest) {
+            if (data != '') {
+                if (uiBlocked == false) {
+                    uiBlocked = true;
+                    Indicator(true);
+                }
+            } else {
+                if (uiBlocked == true) {
+                    uiBlocked = false;
+                    Indicator(false)
+                }
+            }
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            Indicator(false);
+        }
+    })
+
+};
+
+function Indicator(show) {
+    if (show)
+        $('#online-indicator').removeClass('label-danger').addClass('label-success').html('ON AIR');
+    else
+        $('#online-indicator').addClass('label-danger').removeClass('label-success').html('OFFLINE');
+}
